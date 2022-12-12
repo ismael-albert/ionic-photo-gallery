9246e6c7347acec53506b79666abc540ee3f40cc
Ismael Albert <albertismael7@gmail.com>
2022-12-12 09:51:54 -0300

    update

---
+++ src/pages/Tab1.tsx
@@ -0,0 +1,38 @@
+import { IonCol, IonContent, IonFab, IonFabButton, IonGrid, IonHeader, IonIcon, IonImg, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab1.css';
+import { usePhotoGallery, UserPhoto } from '../hooks/usePhotoGallery';
+import { useState } from 'react';
+import { camera } from 'ionicons/icons';
+const Tab1: React.FC = () => {
+  const [photo, setPhoto] = useState<UserPhoto>();
+  const { takePhoto, photos } = usePhotoGallery(); 
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Tab 1</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+      <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab1;
